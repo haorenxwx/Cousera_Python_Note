8885c94982c47b637250fbcd67c48670fb5b65e7
haorenxwx <haorenxwx@users.noreply.github.com>
2018-07-30 23:27:12 +0800

    20180730
    20180730

---
+++ CouseraWebData/Week6/week6_jsonDataProcess.py
@@ -0,0 +1,21 @@
+week6_jsonDataProcess.py
+
+
+import json
+data = '''{
+	"name" : "Chuck",
+	"phone" : {
+	"type" : "int1",
+	"number" : "+1 734 303 4456"
+	},
+	"email" : {
+	"hide" : "yes"
+	}
+}'''
+
+info = json.loads(data)
+print('Name:',info["name"])
+print('Hide',info["email"]["hide"])
+
+Json represents data as nested "lists" and "dictionaries"
+
